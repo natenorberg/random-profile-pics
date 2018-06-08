c3d89e28c2e3d5dc9020144e8de0cb2be43b3313
Nate Norberg <nate.norberg@goquiq.com>
2018-06-08 10:46:08 -0600

    Got single color hash working

--- src/ProfilePic.js
+++ src/ProfilePic.js
@@ -1,7 +1,14 @@
 import * as React from 'react';
+import ColorHash from 'color-hash';
 
 const getInitials = settings =>
   settings.firstName.charAt(0).toUpperCase() + settings.lastName.charAt(0).toUpperCase();
+
+const colorHash = new ColorHash();
+
+const getBackground = settings => {
+  return colorHash.hex(settings.username);
+};
 
 const ProfilePic = ({settings}) => (
   <div
@@ -10,7 +17,7 @@ const ProfilePic = ({settings}) => (
       width: 150,
       height: 150,
       borderRadius: '100%',
-      background: '#888',
+      background: getBackground(settings),
       margin: 16,
       display: 'flex',
       justifyContent: 'center',
